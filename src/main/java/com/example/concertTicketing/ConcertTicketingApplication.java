d683e42b3857bf19a9d0a0f6e6cd9de60b510d77
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-05-07 00:04:48 +0900

    Project init

---
+++ src/main/java/com/example/concertTicketing/ConcertTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertTicketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertTicketingApplication.class, args);
+    }
+
+}
