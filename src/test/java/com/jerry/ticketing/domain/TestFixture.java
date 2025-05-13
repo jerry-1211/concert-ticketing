c4788b09c7570258e3b110aacfe109269e577e93
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-05-13 19:02:12 +0900

    Feat: 테스트를 위한 TestFixture 클래스 구현
    - 모든 테스트에서 일관된 테스트 데이터 사용 가능
    - 테스트 코드 중복 감소 및 가독성 향상

---
+++ src/test/java/com/jerry/ticketing/domain/TestFixture.java
@@ -0,0 +1,123 @@
+package com.jerry.ticketing.domain;
+
+import com.jerry.ticketing.domain.concert.Concert;
+import com.jerry.ticketing.domain.member.Address;
+import com.jerry.ticketing.domain.member.Member;
+import com.jerry.ticketing.domain.payment.Payment;
+import com.jerry.ticketing.domain.payment.enums.PaymentMethod;
+import com.jerry.ticketing.domain.payment.enums.PaymentStatus;
+import com.jerry.ticketing.domain.reservation.Reservation;
+import com.jerry.ticketing.domain.reservation.enums.ReservationStatus;
+import com.jerry.ticketing.domain.seat.ConcertSeat;
+import com.jerry.ticketing.domain.seat.Seat;
+import com.jerry.ticketing.domain.seat.Section;
+import com.jerry.ticketing.domain.seat.enums.ConcertSeatStatus;
+import com.jerry.ticketing.domain.seat.enums.SeatType;
+
+import java.time.LocalDate;
+
+public class TestFixture {
+
+    /**
+     * 더미 데이터
+     */
+
+
+    // 멤버 데이터 생성
+    public static Member createMember() {
+        return createMember("Jerry");
+    }
+
+    public static Member createMember(String name) {
+        return Member.builder()
+                .name(name) // name 필수값 추가
+                .address(Address.of("경기도","고양시"))
+                .email("jerry@naver.com")
+                .phoneNumber("010-2304-4302")
+                .pw("password") // pw 필수값 추가
+                .build();
+    }
+
+
+    // 콘서트 데이터 생성
+    public static Concert createConcert() {
+        return Concert.builder()
+                .title("Cold Play")
+                .date(LocalDate.now())
+                .venue("일산 고양시 대화동")
+                .description("Cold Play의 2번째 내한 공연")
+                .maxTicketsPerUser(2)
+                .build();
+    }
+
+
+    // 구연 데이터 생성
+    public static Section createSection() {
+        return Section.builder()
+                .zone("A")
+                .capacity(100_000)
+                .remainingSeats(100)
+                .build();
+    }
+
+
+    // 좌석 데이터 생성
+    public static Seat createSeat(){
+        return createSeat(createSection());
+    }
+
+    public static Seat createSeat(Section section) {
+        return Seat.builder()
+                .section(section)
+                .seatRow("A")
+                .number(10)
+                .seatType(SeatType.STANDARD)
+                .build();
+    }
+
+
+    // 콘서트 좌석
+    public static ConcertSeat createConcertSeat() {
+        return createConcertSeat(createConcert(), createSeat());
+    }
+
+    public static ConcertSeat createConcertSeat(Concert concert, Seat seat) {
+        return ConcertSeat.builder()
+                .concert(concert)
+                .seat(seat)
+                .price(100_000)
+                .concertSeatStatus(ConcertSeatStatus.BLOCKED)
+                .build();
+    }
+
+
+    // 예약
+    public static Reservation createReservation(){
+        return createReservation(createMember(), createConcert());
+    }
+
+    public static Reservation createReservation(Member member, Concert concert){
+        return Reservation.builder()
+                .member(member)
+                .concert(concert)
+                .totalPrice(1000)
+                .reservationStatus(ReservationStatus.PENDING)
+                .createdAt(LocalDate.now())
+                .expiresAt(LocalDate.now())
+                .build();
+    }
+
+
+    // 결제
+    public static Payment createPayment(Reservation reservation){
+        return Payment.builder()
+                .reservation(reservation)
+                .paymentMethod(PaymentMethod.KAKAOPAY)
+                .paymentStatus(PaymentStatus.PENDING)
+                .paymentDate(LocalDate.now())
+                .idempotent("TEST-IDEMPOTENT")
+                .amount(3)
+                .build();
+    }
+
+}
