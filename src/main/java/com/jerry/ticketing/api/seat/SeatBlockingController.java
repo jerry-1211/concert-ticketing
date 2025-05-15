68753b1314b76146a9ac10d98a3405a22cf52d82
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-05-16 00:39:33 +0900

    Feat: SeatBlockingController 구현
    - "/api/seats" 기본 경로 설정
    - POST "/api/seats/block" 엔드포인트 구현
    - SeatBlockingService 연동 통한 좌석 블로킹 처리

---
+++ src/main/java/com/jerry/ticketing/api/seat/SeatBlockingController.java
@@ -0,0 +1,41 @@
+package com.jerry.ticketing.api.seat;
+
+
+import com.jerry.ticketing.application.seat.SeatBlockingService;
+import com.jerry.ticketing.domain.seat.ConcertSeat;
+import com.jerry.ticketing.dto.request.SeatBlockingRequest;
+import com.jerry.ticketing.dto.response.SeatBlockingResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/seats")
+@RequiredArgsConstructor
+public class SeatBlockingController {
+    private final SeatBlockingService seatBlockingService;
+
+    @PostMapping("/block")
+    public ResponseEntity<SeatBlockingResponse> blockSeats(@RequestBody SeatBlockingRequest request) {
+
+        List<ConcertSeat> blockedConcertSeats = seatBlockingService.blockSeats(
+                request.getConcertId(),
+                request.getSeatIds(),
+                request.getMemberId()
+        );
+
+        SeatBlockingResponse response = new SeatBlockingResponse(
+                blockedConcertSeats.stream().map(ConcertSeat::getId).collect(Collectors.toList()),
+                blockedConcertSeats.get(0).getBlockedExpireAt()
+        );
+
+        return ResponseEntity.ok(response);
+
+    }
+}
