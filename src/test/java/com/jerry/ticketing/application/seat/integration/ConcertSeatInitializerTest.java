e7c3f2f1028d7feccf596f110a987b02d684a8f8
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-05-21 22:54:53 +0900

    Test: 콘서트별 좌석 초기화 시스템 테스트 코드 구현
    - Unit 테스트 & Integration 테스트

---
+++ src/test/java/com/jerry/ticketing/application/seat/integration/ConcertSeatInitializerTest.java
@@ -0,0 +1,134 @@
+package com.jerry.ticketing.application.seat.integration;
+
+import com.jerry.ticketing.application.seat.ConcertSeatInitializer;
+import com.jerry.ticketing.domain.concert.Concert;
+import com.jerry.ticketing.domain.seat.ConcertSeat;
+import com.jerry.ticketing.domain.seat.Seat;
+import com.jerry.ticketing.domain.seat.Section;
+import com.jerry.ticketing.repository.concert.ConcertRepository;
+import com.jerry.ticketing.repository.seat.ConcertSeatRepository;
+import com.jerry.ticketing.repository.seat.SeatRepository;
+import com.jerry.ticketing.repository.seat.SectionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class ConcertSeatInitializerTest {
+
+    @Mock
+    private ConcertRepository concertRepository;
+
+    @Mock
+    private SeatRepository seatRepository;
+
+    @Mock
+    private SectionRepository sectionRepository;
+
+    @Mock
+    private ConcertSeatRepository concertSeatRepository;
+
+    @InjectMocks
+    private ConcertSeatInitializer concertSeatInitializer;
+
+    private Section mockSection;
+
+    @BeforeEach
+    void setup(){
+        // 기본 Mock 설정
+        Concert mockConcert = mock(Concert.class);
+        mockSection = mock(Section.class);
+        Seat mockSeat = mock(Seat.class);
+
+        when(concertRepository.findById(anyLong())).thenReturn(Optional.of(mockConcert));
+
+        when(seatRepository.findById(anyLong())).thenReturn(Optional.ofNullable(mockSeat));
+
+        when(sectionRepository.save(any(Section.class))).thenReturn(mockSection);
+
+    }
+
+
+    @Test
+    @DisplayName("Section에 매팽된 Concert가 없을 때 Section & ConcertSeat 초기화 메서드 호출")
+    void shouldInitializeSectionAndConcertSeatsWhenConcertNotMapped(){
+
+        // When
+       concertSeatInitializer.initializeSectionAndConcertSeats(1L);
+
+        // Then
+        // 콘서트 존재 여부 확인
+        verify(sectionRepository,times(1)).existsByConcertId(anyLong());
+
+        // Section & ConcertSeat 저장 확인
+        verify(sectionRepository, atLeastOnce()).save(any(Section.class));
+        verify(concertSeatRepository, atLeastOnce()).saveAll(anyList());
+
+    }
+
+    @Test
+    @DisplayName("이미 Section에 매핑된 Concert가 있는 경우 초기화를 건너뜀")
+    void shouldSkipInitializeSectionAndConcertSeatsWhenConcertMapped(){
+        // Given
+        when(sectionRepository.existsByConcertId(anyLong())).thenReturn(true);
+
+        // When
+        concertSeatInitializer.initializeSectionAndConcertSeats(1L);
+
+        // Then
+        verify(sectionRepository,times(1)).existsByConcertId(anyLong());
+        verify(sectionRepository, never()).save(any(Section.class));
+        verify(concertSeatRepository, never()).saveAll(anyList());
+    }
+
+    @Test
+    @DisplayName("Section이 이미 존재할 때 기존 것을 반환 - createSectionIfNotExists 메서드 검증" )
+    void shouldReturnExistingSectionWhenExits(){
+
+        // Given
+        when(sectionRepository.findByConcertId(anyLong()))
+                .thenReturn(Optional.of(mockSection));
+
+        // When
+        concertSeatInitializer.initializeSectionAndConcertSeats(1L);
+
+        // Then
+        verify(sectionRepository, atLeastOnce()).findByConcertId(anyLong());
+        verify(sectionRepository,never()).save(any(Section.class));
+    }
+
+
+    @Test
+    @DisplayName("ConcertSeat 이미 존재할 때 기존 것을 반환 - createConcertSeats 메서드 검증")
+    void shouldReturnExistingConcertSeatWhenExits(){
+        ConcertSeat mockConcertSeat = mock(ConcertSeat.class);
+
+        // Given
+        when(sectionRepository.findByConcertId(anyLong())).thenReturn(Optional.of(mockSection));
+        when(concertSeatRepository.findByConcertId(anyLong())).thenReturn(List.of(mockConcertSeat));
+
+        // When
+        concertSeatInitializer.initializeSectionAndConcertSeats(1L);
+
+        // Then
+        verify(sectionRepository, atLeastOnce()).findByConcertId(anyLong());
+        verify(concertSeatRepository,never()).saveAll(anyList());
+
+
+    }
+
+}
