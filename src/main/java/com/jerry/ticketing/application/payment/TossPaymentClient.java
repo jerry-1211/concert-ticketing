64687d8d17bb038d748d9db001197a76dbadc1a8
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-05-28 15:50:41 +0900

    Feat: TossPaymentClient 구현 및 결제 연동 핵심 로직 추가
    - Toss API와의 통신을 위한 핵심 로직 구현
    - 요청/응답 처리를 위한 관련 DTO 클래스 작성

---
+++ src/main/java/com/jerry/ticketing/application/payment/TossPaymentClient.java
@@ -0,0 +1,45 @@
+package com.jerry.ticketing.application.payment;
+
+import com.jerry.ticketing.config.payment.TossPaymentConfig;
+import com.jerry.ticketing.dto.request.TossPaymentConfirmRequest;
+import com.jerry.ticketing.dto.response.TossPaymentResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TossPaymentClient {
+
+    private final TossPaymentConfig tossPaymentConfig;
+
+    public TossPaymentResponse confirmPayment(String paymentKey, String orderId, String amount) {
+
+        WebClient webClient = WebClient.create();
+
+        String encodeAuth = Base64.getEncoder()
+                .encodeToString((tossPaymentConfig.getTestClientApiKey() + ":").getBytes(StandardCharsets.UTF_8));
+
+        TossPaymentConfirmRequest request = TossPaymentConfirmRequest.builder()
+                .paymentKey(paymentKey)
+                .orderId(orderId)
+                .amount(amount)
+                .build();
+
+        return webClient.post()
+                .uri(tossPaymentConfig.getBaseUrl() + "/confirm")
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + encodeAuth)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(TossPaymentResponse.class)
+                .block();
+    }
+}
