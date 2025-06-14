6d4596b0b8689a4f561080400b93cbc45214517d
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-06-14 22:25:49 +0900

    Refactor: OrderId 생성 강화

--- src/main/java/com/jerry/ticketing/payment/util/PaymentOrderIdGenerator.java
+++ src/main/java/com/jerry/ticketing/payment/util/PaymentOrderIdGenerator.java
@@ -3,8 +3,18 @@ package com.jerry.ticketing.payment.util;
 import java.util.UUID;
 
 public class PaymentOrderIdGenerator {
+    private static final String PREFIX = "ORDER";
+    private static final int RANDOM_LENGTH = 8;
+
+    private PaymentOrderIdGenerator() {
+    }
+
     public static String generate(Long reservationId) {
-        return "RES-" + reservationId + "-" + UUID.randomUUID().toString().substring(0, 8);
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        String randomPart = UUID.randomUUID().toString().replace("-", "").substring(0, RANDOM_LENGTH);
+        String hashedReservationId = String.valueOf(Math.abs(reservationId.hashCode()));
+
+        return String.format("%s-%s-%s-%s", PREFIX, timestamp, hashedReservationId, randomPart);
     }
 
 }
