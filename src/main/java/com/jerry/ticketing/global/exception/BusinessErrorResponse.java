0cc9ffcd5be974616d963df7eae13835174f45af
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-07-04 19:17:48 +0900

    Fix: GlobalExceptionHandler 에러 수정
    - `errorCode.name()`을 이상하게 작성하여 예외가 터질때 500(서버 에러) 발생

--- src/main/java/com/jerry/ticketing/global/exception/BusinessErrorResponse.java
+++ src/main/java/com/jerry/ticketing/global/exception/BusinessErrorResponse.java
@@ -1,8 +1,10 @@
 package com.jerry.ticketing.global.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class BusinessErrorResponse {
     private String code;
     private String message;
--- src/main/java/com/jerry/ticketing/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jerry/ticketing/global/exception/GlobalExceptionHandler.java
@@ -11,8 +11,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<BusinessErrorResponse> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
-        BusinessErrorResponse businessErrorResponse = new BusinessErrorResponse("BUSINESS_ERROR", errorCode.getMessage());
+        BusinessErrorResponse businessErrorResponse = new BusinessErrorResponse(errorCode.name(), errorCode.getMessage());
+
+        if ((errorCode == SeatErrorCode.SEAT_NOT_FOUND) || (errorCode == SeatErrorCode.SEAT_ALREADY_BLOCKED)) {
+            return new ResponseEntity<>(businessErrorResponse, HttpStatus.CONFLICT);
+        }
+
         return new ResponseEntity<>(businessErrorResponse, HttpStatus.BAD_REQUEST);
+
     }
+
 
 }
