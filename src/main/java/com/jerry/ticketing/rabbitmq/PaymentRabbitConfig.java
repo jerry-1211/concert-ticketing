17ff9743192457a052fe97a6e0edb6340fe90343
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-07-07 23:16:25 +0900

    Feat: RabbitMQ Payment Consumer, Publisher & Configuration 구현

---
+++ src/main/java/com/jerry/ticketing/rabbitmq/PaymentRabbitConfig.java
@@ -0,0 +1,52 @@
+package com.jerry.ticketing.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class PaymentRabbitConfig {
+
+    @Bean
+    public TopicExchange paymentExchange() {
+        return new TopicExchange("payment.exchange");
+    }
+
+    @Bean
+    public Queue paymentQueue() {
+        return QueueBuilder.durable("payment.process.queue")
+                .withArgument("x-single-active-consumer", true)
+                .build();
+    }
+
+    @Bean
+    public Binding paymentBinding() {
+        return BindingBuilder.bind(paymentQueue())
+                .to(paymentExchange())
+                .with("payment.*");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
+    }
+
+    @Bean
+    public RabbitListenerContainerFactory<SimpleMessageListenerContainer> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+
+}
