9b2f92604b3ad1547f3f1ca445dfaf6ddfff521f
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-07-18 12:58:17 +0900

    Chore: 기타 문서 수정 및 안쓰는 메서드 제거

--- src/main/java/com/jerry/ticketing/seat/infrastructure/repository/SectionRepository.java
+++ src/main/java/com/jerry/ticketing/seat/infrastructure/repository/SectionRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SectionRepository extends JpaRepository<Section,Long> {
-    Optional<Section> findByZone(String zone);
+public interface SectionRepository extends JpaRepository<Section, Long> {
 
     Optional<Section> findByConcertId(Long ConcertId);
 
